df442c042018a3bd33c0e315f7f0e3c70056f0b2
albertgoncalves <goncalves.albert@gmail.com>
2019-07-23 18:33:38 -0400

    sneaking these in...

--- src/lexer.mll
+++ src/lexer.mll
@@ -1,9 +1,11 @@
 {
     open Parser
     exception Error of string
+    exception Eof
     let unexpected_char (lexbuf : Lexing.lexbuf) : Parser.token =
         let message =
-            Printf.sprintf "Line %d, offset %d: unexpected character.\n"
+            Printf.sprintf
+                "Lexer Error: unexpected character\nline %d, offset %d"
                 lexbuf.lex_curr_p.pos_lnum (Lexing.lexeme_start lexbuf) in
         raise (Error (message))
 }
@@ -11,20 +13,7 @@
 let digit = ['0'-'9']
 let float = digit* '.'? digit+ | digit+ '.'? digit*
 
-(* This rule looks for a single line, terminated with '\n' or eof.  It returns
-   a pair of an optional string (the line that was found) and a Boolean flag
-   (false if eof was reached). *)
-rule line = parse
-    | ([^'\n']* '\n') as line   { Some line, true } (* Normal case: one line,
-                                                       no eof. *)
-    | eof                       { None, false }     (* Normal case: no data,
-                                                       eof. *)
-    | ([^'\n']+ as line) eof    { Some (line ^ "\n"), false }
-        (* Special case: some data but missing '\n', then eof.  Consider this
-           as the last line, and add the missing '\n'. *)
-
-(* This rule analyzes a single line and turns it into a stream of tokens. *)
-and token = parse
+rule token = parse
     | [' ' '\t']                { token lexbuf }
     | '\n'                      { EOL }
     | float as i                { FLOAT (float_of_string i) }
@@ -34,4 +23,5 @@ and token = parse
     | '/'                       { DIV }
     | '('                       { LPAREN }
     | ')'                       { RPAREN }
+    | eof                       { raise Eof }
     | _                         { unexpected_char lexbuf }
--- src/main.ml
+++ src/main.ml
@@ -4,35 +4,28 @@ module P = Parser.MenhirInterpreter
 let succeed : float -> unit = Printf.printf "%f\n%!"
 
 let fail (lexbuf : Lexing.lexbuf) (checkpoint : float P.checkpoint) : unit =
-    match checkpoint with
+    (match checkpoint with
         | P.HandlingError env ->
             let s = P.current_state_number env in
-            Messages.message s |> prerr_string;
-            Printf.fprintf stderr "ln %d\n%!" lexbuf.lex_curr_p.pos_lnum
+            Messages.message s
+            |> Printf.fprintf stderr "ln %d\n%s%!" lexbuf.lex_curr_p.pos_lnum
         | _ ->
             Printf.fprintf stderr
-                "Line %d, offset %d: syntax error.\n%!"
-                lexbuf.lex_curr_p.pos_lnum (Lexing.lexeme_start lexbuf)
+                "Parser Error: unhandled condition\nline %d, offset %d"
+                lexbuf.lex_curr_p.pos_lnum (Lexing.lexeme_start lexbuf));
+    exit 1
 
 let loop (lexbuf : Lexing.lexbuf) (result : float P.checkpoint) : unit =
     let supplier : unit -> P.token * M.position * M.position =
         P.lexer_lexbuf_to_supplier Lexer.token lexbuf in
     P.loop_handle succeed (fail lexbuf) supplier result
 
-let process : (string option -> unit) = function
-    | None ->
-        ()
-    | Some line ->
-        let lexbuf : Lexing.lexbuf = Lexing.from_string line in
-        try
+let () =
+    try
+        let lexbuf : Lexing.lexbuf = Lexing.from_channel stdin in
+        while true do
             loop lexbuf (Parser.Incremental.main lexbuf.lex_curr_p)
-        with Lexer.Error msg ->
-            Printf.fprintf stderr "%s%!" msg
-
-let rec repeat (channel : Lexing.lexbuf) : unit =
-    let (optional_line, continue) = Lexer.line channel in
-    process optional_line;
-    if continue then
-        repeat channel
-
-let () = repeat (Lexing.from_channel stdin)
+        done
+    with
+        | Lexer.Eof -> exit 0
+        | Lexer.Error msg -> Printf.fprintf stderr "%s%!" msg; exit 1
