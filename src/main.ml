f4b0541b9d0e318acc2c20b67c19a800ca545ebc
albertgoncalves <goncalves.albert@gmail.com>
2019-07-23 21:56:59 -0400

    simplify

--- src/main.ml
+++ src/main.ml
@@ -1,20 +1,17 @@
 let print_error (lexbuf : Lexing.lexbuf) (handle : string) : unit =
-    Printf.fprintf
-        stderr
+    Printf.eprintf
         "Line %d, offset %d: %s error\n%!"
         lexbuf.lex_start_p.pos_lnum
         (lexbuf.lex_start_p.pos_cnum - lexbuf.lex_start_p.pos_bol)
-        handle;
-    exit 1
+        handle
 
 let () =
+    let lexbuf : Lexing.lexbuf = Lexing.from_channel stdin in
     try
-        let lexbuf : Lexing.lexbuf = Lexing.from_channel stdin in
         while true do
-            try
-                Printf.printf "%f\n%!" (Parser.main Lexer.token lexbuf)
-            with
-                | Lexer.Error -> print_error lexbuf "Lexer"
-                | Parser.Error -> print_error lexbuf "Parser"
+            Printf.printf "%f\n%!" (Parser.main Lexer.token lexbuf)
         done
-    with Lexer.Eof -> exit 0
+    with
+        | Lexer.Eof -> exit 0
+        | Lexer.Error -> print_error lexbuf "Lexer"; exit 1
+        | Parser.Error -> print_error lexbuf "Parser"; exit 1
