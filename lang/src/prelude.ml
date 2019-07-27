670063e743c9f1a490ade5b62d961e3c3d2d535e
albertgoncalves <goncalves.albert@gmail.com>
2019-07-27 00:06:06 -0400

    pprint to lang/

--- lang/src/prelude.ml
+++ lang/src/prelude.ml
@@ -5,14 +5,41 @@ type expr =
     | Fn of string * string list * expr list
     | Assign of expr * expr
 
-let rec string_of_expr: (expr -> string) = function
+let rec indent (margin : string) (x : string) : (int -> string) = function
+    | 0 -> x
+    | offset -> indent margin (Printf.sprintf "%s%s" margin x) (offset - 1)
+
+let string_of_header : (string -> string -> string) =
+    Printf.sprintf "fn %s (%s) {"
+
+let rec string_of_expr (limit : int) (margin : string) (offset : int)
+    : (expr -> string) = function
     | Assign (x, y) ->
-        Printf.sprintf "Assign(%s,%s)" (string_of_expr x) (string_of_expr y)
-    | Id x -> Printf.sprintf "Id(%s)" x
-    | Str x -> Printf.sprintf "Str(%s)" x
-    | Num x -> Printf.sprintf "Num(%s)" x
+        Printf.sprintf "%s = %s;"
+            (string_of_expr limit margin offset x)
+            (string_of_expr limit margin offset y)
+    | Id x -> Printf.sprintf "%s" x
+    | Str x -> Printf.sprintf "\"%s\"" x
+    | Num x -> Printf.sprintf "%s" x
     | Fn (x, xs, ys) ->
-        Printf.sprintf "Fn(%s)(%s)(%s)"
-            x
-            (String.concat "," xs)
-            (List.map string_of_expr ys |> String.concat ",")
+        let block : string =
+            List.map (string_of_expr limit margin (offset + 1)) ys
+            |> String.concat
+                (indent margin "" offset |> Printf.sprintf "\n%s") in
+        let args : string = String.concat ", " xs in
+        let header: string =
+            let header : string = string_of_header x args in
+            if (String.length header) + (4 * (offset - 1)) < limit then
+                header
+            else
+                let args : string list =
+                    List.map (fun xs -> indent margin xs offset) xs in
+                Printf.sprintf
+                    "\n%s\n%s"
+                    (String.concat ",\n" args)
+                    (indent margin "" (offset - 1))
+                |> string_of_header x in
+        Printf.sprintf "%s\n%s\n%s"
+            header
+            (indent margin block offset)
+            (indent margin "}" (offset - 1))
