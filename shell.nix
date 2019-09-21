cbed353ac4fe0710b18190086514087713ab93e2
albertgoncalves <goncalves.albert@gmail.com>
2019-09-21 16:06:10 -0400

    ha

--- shell.nix
+++ shell.nix
@@ -2,6 +2,12 @@ with import <nixpkgs> {};
 mkShell {
     buildInputs = [
         (with ocaml-ng.ocamlPackages_4_07; [
+            async   #
+            cohttp  #
+            core    #
+            findlib #
+            lwt4    #
+            utop    #
             menhir
             ocaml
             ocp-indent
