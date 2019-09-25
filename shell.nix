d21177e79dbd61ce0fe86f2db25f70e3b89dfaf8
albertgoncalves <goncalves.albert@gmail.com>
2019-09-24 20:59:12 -0400

    resolve merge conflict

--- shell.nix
+++ shell.nix
@@ -4,9 +4,9 @@ mkShell {
         (with ocaml-ng.ocamlPackages_4_07; [
             async   #
             cohttp  #
-            core    #
-            findlib #
-            lwt4    #
+            core    # https://www.cs.cornell.edu/courses/cs3110/2019sp/textbook/ads/concurrency.html
+            findlib # https://dev.realworldocaml.org/concurrent-programming.html
+            lwt4    # https://github.com/pusher/websockets-from-scratch-tutorial
             utop    #
             menhir
             ocaml
