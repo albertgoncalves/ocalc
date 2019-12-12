38298b092bda59bc3e639a83b5fcb56864336158
albertgoncalves <goncalves.albert@gmail.com>
2019-12-12 17:24:32 -0500

    remove unused deps

--- shell.nix
+++ shell.nix
@@ -2,12 +2,6 @@ with import <nixpkgs> {};
 mkShell {
     buildInputs = [
         (with ocaml-ng.ocamlPackages_4_07; [
-            async   #
-            cohttp  #
-            core    # https://www.cs.cornell.edu/courses/cs3110/2019sp/textbook/ads/concurrency.html
-            findlib # https://dev.realworldocaml.org/concurrent-programming.html
-            lwt4    # https://github.com/pusher/websockets-from-scratch-tutorial
-            utop    #
             menhir
             ocaml
             ocp-indent
